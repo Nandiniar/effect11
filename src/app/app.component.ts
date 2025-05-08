41cfa4c0f58e508f441dcb1930bafffb6810421b
nandini <nandani@contaque.com>
2025-05-08 10:54:10 +0530

    Add the changes
    Signed-off-by: nandini <nandani@contaque.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,signal,effect } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -10,4 +10,26 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'effect11';
   //  effect is use with signal
+  //  jab bhi signal change hota hai toh effect ke andar notification milte hai ke signal change hua hai
+  userName=signal('Anil');
+  count=signal(0);
+  displayHeading=false;
+  constructor(){
+    effect(()=>{
+console.log(this.userName()); // jab bhi signal update hoga tab hume dekhega yeh
+if(this.count()==1){
+  this.displayHeading=true;  // if we want ke time 2 sec ke baad na dekhe
+  setTimeout(()=>{
+    this.displayHeading=false
+  },2000)
+}
+else{
+  this.displayHeading=false;
+}
+    })
+  }
+  toggleValue(){
+    this.count.set(this.count()+1)
+  
+  }
 }
